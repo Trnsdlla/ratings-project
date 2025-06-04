c8933816d437ec49bf96ffad365ab11df920b7a4
Michael Trandella <107638701+Trnsdlla@users.noreply.github.com>
2025-06-04 16:18:49 -0400

    Add POST API route for projects

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -15,3 +15,23 @@ const dummyProjects =
 export async function GET() {
   return NextResponse.json(dummyProjects);
 }
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { title, description } = body;
+
+    const newProject = {
+      id: dummyProjects.length + 1,
+      title: title,
+      description: description,
+    };
+
+    dummyProjects.push(newProject);
+    
+    return NextResponse.json({message: 'Project created', project: newProject });
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : 'Unkown error';
+    return NextResponse.json({ error: message });
+  }
+}
